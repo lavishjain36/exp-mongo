500e6f7f72fc68a8cd9f8b851c697fce1d88003e
lavish <lavishjain36@gmail.com>
2022-06-20 10:38:38 +0530

    g

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,30 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.get('/all',async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl)
+  try 
+  {
+    const db = await client.db('b30wd')
+    let users = await db.collection('users').find().toArray();
+    res.json({
+      statusCode:200,
+      users
+    })
+  } catch (error) 
+  {
+    console.log(error)
+    res.json({
+      statusCode:500,
+      message:"Internal server Error"
+    })
+  }
+  finally
+  {
+    client.close();
+  }
+})
+
 router.post('/signup', async(req, res)=> {
   const client = await  MongoClient.connect(dbUrl);
 try{
@@ -38,4 +62,86 @@ try{
   client.close()
 }
 });
+
+//if email is already exist,Then login
+router.post('/login', async(req, res)=> {
+  const client = await  MongoClient.connect(dbUrl);
+try{
+  const db= await client.db('b30wd');
+  let user=await db.collection('users').find({email:req.body.email}).toArray();
+  if(user.length>0){
+    let user=await db.collection('users').findOne({email:req.body.email});
+  if(user.password==req.body.password)
+ {
+   res.json({
+     statusCode:200,
+     message:'Login Successfully',
+   });
+ }else{
+    res.json({
+      statusCode:400,
+      message:'Invalid credentials'
+    });
+ }
+  }else{
+    res.json({
+      statusCode:400,
+      message:'User does not exists'
+    });
+  }
+
+}catch(err){
+  console.log(error)
+  res.json({
+    statusCode:500,
+    message:"Internal Server Error"
+  })
+
+}finally{
+  client.close()
+}
+});
+router.put('/change-password',async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl)
+  try 
+  {
+    const db = await client.db('b30wd')
+    let users = await db.collection('users').find({email:req.body.email}).toArray();
+    if(users.length>0)
+    {
+      
+      if(users[0].password===req.body.oldPassword)
+      {
+        let update = await db.collection('users').updateOne({email:req.body.email},{$set:{password:req.body.newPassword}})
+        res.json({
+          statusCode:200,
+          message:"Password Updated Successfully"
+        })
+      }
+      else{
+        res.json({
+          statusCode:400,
+          message:"Invalid Credentials"
+        })
+      }
+    }
+    else{
+      res.json({
+        statusCode:400,
+        message:"User does not exists"
+      })
+    }
+  } catch (error) 
+  {
+    console.log(error)
+    res.json({
+      statusCode:500,
+      message:"Internal server Error"
+    })
+  }
+  finally
+  {
+    client.close();
+  }
+})
 module.exports = router;
